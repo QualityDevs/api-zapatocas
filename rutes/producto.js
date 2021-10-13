10c6fdfc137432f9678ffc22c9a193eb9a28a5f6
John Garcia <john_garcia@javeriana.edu.co>
2021-10-13 09:03:00 -0500

    Rutas de productos

--- rutes/producto.js
+++ rutes/producto.js
@@ -1,26 +1,68 @@
-const express = require('express');
-var ObjectID=require('mongodb').ObjectID;
+import  Express  from 'express';
+import {getDB } from '../db/conn.js';
 
+const rutasProducto = Express.Router();
 
-const recordRoutes = express.Router();
-
-const dbo = require('../db/conn')
-
-recordRoutes.route('/producto').get(async function (req,res){
-    const dbConnect = dbo.getDb();
-
-    dbConnect
-    .collection('productos')
-    .find({})
-    .limit(50)
-    .toArray(function (err, result){
-        if (err){
-            res.status(400).send('Error al buscar Vehiculos');
-        }else{
-            res.json(result);
+rutasProducto.route('/productos').get((req, res) => {
+    getDB().collection('productos').find({}).toArray((error, result) => {
+        if (error) {
+            res.sendStatus(500).send('Error buscando productos');
+        } else {
+            res.json(result)
         }
     });
-
 });
 
-module.exports = recordRoutes;
+rutasProducto.route('/productos/delete').delete((req, res) =>{
+    const edit = req.body;
+    const filtro = {_id: new ObjectId(edit.id)};
+    getBD().collection('productos').deleteOne(filtro, (err, result) => {
+        if (err) {
+            res.sendStatus(500).send('Error buscando productos');
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
+
+rutasProducto.route('/productos/edit').patch((req, res)=>{
+    const edit = req.body;
+    const filtro = {_id: new ObjectId(edit.id)};
+    delete edit.id;
+    const operacion={$set: edit};
+     getBD().collection('productos').findOneAndUpdate(filtro, operacion,{upsert: true}, (error, result) => {
+        if (error) {
+            console.error(error);
+            res.sendStatus(500);
+        } else {
+            console.log("Actualizadosssss");
+            res.sendStatus(200);
+        }
+     });
+});
+
+rutasProducto.route('/productos/new').post((req, res) => {
+    const datosproducto = req.body;
+    try {
+        if (Object.keys(datosproducto).includes('nombre') &&
+            Object.keys(datosproducto).includes('descripcion') &&
+            Object.keys(datosproducto).includes('vunitario') &&
+            Object.keys(datosproducto).includes('estado')) {
+            getDB().collection('productos').insertOne(datosproducto, (error, result) => {
+                if (error) {
+                    console.error(error);
+                    res.sendStatus(500);
+                } else {
+                    console.log(result);
+                    res.status(200).send(result);
+                }
+            });
+        } else {
+            res.sendStatus(500);
+        }
+    } catch {
+       res.sendStatus(500);
+    }
+});
+
+export default rutasProducto;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import Express from 'express';
 import Cors from 'cors';
-import { conectarBD, getDB } from './db/conn.js';
+import { conectarBD } from './db/conn.js';
 import dotenv from 'dotenv';
+import rutasProducto from './rutes/producto.js';
+
 
 dotenv.config({ path: './config.env'});
 
@@ -9,71 +11,7 @@ const app = Express();
 
 app.use(Express.json());
 app.use(Cors());
-
-app.get('/productos', (req, res) => {
-    getDB().collection('productos').find({}).toArray((error, result) => {
-        if (error) {
-            res.sendStatus(500).send('Error buscando productos');
-        } else {
-            res.json(result)
-        }
-    });
-});
-
-app.delete('/productos/delete', (req, res) =>{
-    const edit = req.body;
-    const filtro = {_id: new ObjectId(edit.id)};
-    getBD().collection('productos').deleteOne(filtro, (err, result) => {
-        if (err) {
-            res.sendStatus(500).send('Error buscando productos');
-        } else {
-            res.sendStatus(200);
-        }
-    });
-});
-
-
-app.patch('/productos/edit', (req, res)=>{
-    const edit = req.body;
-    const filtro = {_id: new ObjectId(edit.id)};
-    delete edit.id;
-    const operacion={$set: edit};
-     getBD().collection('productos').findOneAndUpdate(filtro, operacion,{upsert: true}, (error, result) => {
-        if (error) {
-            console.error(error);
-            res.sendStatus(500);
-        } else {
-            console.log("Actualizadosssss");
-            res.sendStatus(200);
-        }
-     });
-
-});
-
-app.post('/productos/new', (req, res) => {
-    const datosproducto = req.body;
-    try {
-        if (Object.keys(datosproducto).includes('nombre') &&
-            Object.keys(datosproducto).includes('descripcion') &&
-            Object.keys(datosproducto).includes('vunitario') &&
-            Object.keys(datosproducto).includes('estado')) {
-            getDB().collection('productos').insertOne(datosproducto, (error, result) => {
-                if (error) {
-                    console.error(error);
-                    res.sendStatus(500);
-                } else {
-                    console.log(result);
-                    res.status(200).send(result);
-                }
-            });
-        } else {
-            res.sendStatus(500);
-        }
-    } catch {
-       res.sendStatus(500);
-    }
-});
-
+app.use(rutasProducto);
 
 
 const main = () => {
