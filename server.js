97844c6534dc6f2230c50cff4ce961965364cbcf
sergio <seanviar@hotmail.com>
2021-10-22 14:28:50 -0500

    autenticacion para el back

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ import { conectarBD } from './db/conn.js';
 import dotenv from 'dotenv';
 import rutasProducto from './rutes/producto.js';
 import rutasUser from './rutes/usuarios.js';
-
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa'; 
 
 dotenv.config({ path: './config.env'});
 
@@ -12,6 +13,21 @@ const app = Express();
 
 app.use(Express.json());
 app.use(Cors());
+
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://autenticacionmintic.us.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'api-auteticacion-zapatocas-mintic',
+  issuer: 'https://autenticacionmintic.us.auth0.com/',
+  algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
+
 app.use(rutasProducto);
 app.use(rutasUser)
 
